bf96dbcd0f2042d82a2ff1d9e8b9180fbe74a597
GregoryDavidDev <150613235+GregoryDavidDev@users.noreply.github.com>
2024-01-18 12:34:28 +0100

    polished the portfolio even more

--- src/components/pages/IntroductionPage.js
+++ src/components/pages/IntroductionPage.js
@@ -1,5 +1,6 @@
 import './IntroductionPage.css'
 import './Page.css'
+import '../../Icons.css'
 import Cursor from "../Cursor";
 import ProjectCard from "../ProjectCard";
 import ProjectShelf from "../ProjectShelf";
@@ -15,6 +16,7 @@ function IntroductionPage() {
     return <div className="page-base">
         <div className="cta-box">
             <div className="cta-box-part cta-box-left">
+                <a className="icon-linkedin" href="https://www.linkedin.com/in/gr%C3%A9gory-david-b1732029b/"></a>
                 <h1 className="text-title">Je m'appelle <b>Grégory</b>, et je prévois d'être <b>Développeur C#</b><Cursor/></h1>
                 <p>Je suis passionné d'informatique depuis toujours, et je fais du développement de logiciels et de jeux dans mon temps libre.</p>
                 <p>Le C# est mon langage de prédilection depuis mes débuts dans la programmation.</p>
