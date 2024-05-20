1014e02fad4cab0b6a44b281e13808ab43a4c1b8
GregoryDavidDev <150613235+GregoryDavidDev@users.noreply.github.com>
2024-05-20 10:13:10 +0200

    connected project bento layout to pantheon

--- src/components/ProjectBentoCard.jsx
+++ src/components/ProjectBentoCard.jsx
@@ -1,17 +1,24 @@
 import './ProjectBentoCard.css'
 import Default from '../res/cards/default/logo.png'
 import TagElement from "./TagElement";
+import React from "react";
+import {pantheonBaseUrl} from "../lib/pantheon";
 
-export function ProjectBentoCard({fill}) {
+export function ProjectBentoCard({project, fill}) {
+    if (!project) return null;
+
     return <div className={fill ? "bento-card bento-fill" : "bento-card"}>
         <div className="bento-card-inside">
-            <p className="title">Project Name</p>
-            <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisl magna, mollis vel pulvinar eleifend, congue sit amet lacus. Cras vel nisl a velit rhoncus volutpat in eu ipsum. Quisque ultricies massa quis diam convallis, quis tempor risus semper. Duis porttitor odio non justo feugiat luctus. Donec dignissim tristique venenatis. Praesent fringilla dapibus magna, cursus blandit mi sollicitudin ut. Duis dignissim semper nibh, vehicula lobortis lacus vulputate sit amet. Nulla accumsan magna nec lectus fermentum egestas. Nulla ultricies euismod diam, in ultrices nisi auctor vitae. Duis feugiat quam purus, maximus bibendum erat imperdiet id. Sed pulvinar mauris ac diam laoreet, nec scelerisque ligula consectetur. Sed rutrum pharetra elementum. Nullam sollicitudin commodo turpis, eu vulputate neque aliquam sed.</p>
-            <img className="picture" alt="Project cover" src={Default}></img>
+            <p className="title">{project.name}</p>
+            <p className="text">{project.summary}</p>
+            <img className="picture" alt="Project cover" src={`${pantheonBaseUrl}/project/${project.slug}/logo`}></img>
             <div className="footer">
-                <TagElement name="project"></TagElement>
-                <TagElement name="project"></TagElement>
-                <TagElement name="project"></TagElement>
+                <TagElement name={project.language}/>
+                {
+                    project.tags.map((answer, i) => {
+                        return (<TagElement name={answer}/>)
+                    })
+                }
             </div>
         </div>
     </div>
--- src/components/pages/ProjectsPage.jsx
+++ src/components/pages/ProjectsPage.jsx
@@ -1,19 +1,37 @@
 import {ProjectBentoCard} from "../ProjectBentoCard";
+import {useEffect, useState} from "react";
+import {fetchPantheonProjects, fetchPantheonProjectsByCategory} from "../../lib/pantheon";
 
 
 export function ProjectsPage() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        fetchPantheonProjects()
+            .then(async data => {
+                setProjects(await data.json())
+            })
+    }, []);
+
+    if (projects.length === 0) {
+        return <p>Loading</p>
+    }
+
+    let finalArray = projects;
+    if (finalArray.length > 4) finalArray = projects.slice(0, 4);
+
     return <div className="page-base left-header-page">
         <div className="left-header-text-wrap">
             <h1 className="left-header-text">PROJETS</h1>
         </div>
         <div className="left-header-fill bento-card-container">
             <div className="bento-card-row">
-                <ProjectBentoCard fill={true}/>
-                <ProjectBentoCard/>
+                <ProjectBentoCard project={finalArray[0]} fill={true}/>
+                <ProjectBentoCard project={finalArray[1]}/>
             </div>
             <div className="bento-card-row">
-                <ProjectBentoCard/>
-                <ProjectBentoCard fill={true}/>
+                <ProjectBentoCard project={finalArray[2]}/>
+                <ProjectBentoCard project={finalArray[3]} fill={true}/>
             </div>
         </div>
     </div>
